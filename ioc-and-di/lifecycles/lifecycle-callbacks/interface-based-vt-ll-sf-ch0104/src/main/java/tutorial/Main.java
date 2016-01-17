cde9fc98a5236367326c5519e506b4bae5b40ffc
Nuwan Chamara <nuwanlanka@gmail.com>
2016-01-17 08:41:06 +0530

    ioc-and-di _ lifecycles _ lifecycle-callbacks _ interface-based-vt-ll-sf-ch0104

---
+++ ioc-and-di/lifecycles/lifecycle-callbacks/interface-based-vt-ll-sf-ch0104/src/main/java/tutorial/Main.java
@@ -0,0 +1,17 @@
+package tutorial;
+
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+public class Main {
+
+    public static void main(String[] args) throws Throwable {
+        AnnotationConfigApplicationContext annotationConfigApplicationContext =
+                new AnnotationConfigApplicationContext(Astronaut.class.getPackage().getName());
+
+        // NB important!
+        annotationConfigApplicationContext.registerShutdownHook();
+
+    }
+}
