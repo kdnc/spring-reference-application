c8429cb55e3e2c3d2599093959b6215e5f4e6d56
Nuwan Chamara <nuwanlanka@gmail.com>
2016-01-17 08:37:31 +0530

    ioc-and-di _ context-configuration _ component-scan _ component-scan-vt-ll-sf-ch0104

---
+++ ioc-and-di/context-configuration/component-scan/component-scan-vt-ll-sf-ch0104/src/main/java/tutorial/Main.java
@@ -0,0 +1,11 @@
+package tutorial;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) throws Throwable {
+        String pkgName = Config.class.getPackage().getName();
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(pkgName);
+        ac.registerShutdownHook();
+    }
+}
